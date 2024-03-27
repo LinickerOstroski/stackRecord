e9a519db83eea89f0a14e89fe4afb1f6f99758e3
Linicker <olinicker@gmail.com>
2024-03-27 14:48:06 -0300

    commit inicial

---
+++ README.txt
@@ -0,0 +1,12 @@
+Stacks in Java.
+
+Programa que recebe dados de um recorde e adiciona à uma pilha,
+realizado em javaForms. Sinta-se a vontade para clonar o projeto e melhora-lo.
+
+Desenvolvido por: Línicker Ostroski Mota.
+
+
+
+
+
+
---
+++ src/main/java/pacoteprincipal/StacksJavaPraticaI.java
@@ -0,0 +1,14 @@
+package pacoteprincipal;
+
+/**
+ *
+ * @author Iksortso K2
+ */
+public class StacksJavaPraticaI {
+
+    public static void main(String[] args) {
+        FormularioPrincipal form = new FormularioPrincipal();
+        form.setVisible(true);
+         
+    }
+}
